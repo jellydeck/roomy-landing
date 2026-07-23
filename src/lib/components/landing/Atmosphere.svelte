31fb0b98f5bf23f8d8f6ec12e2a134c95903ae3b
Jaydip Sanghani <91427591+jellydeck@users.noreply.github.com>
2026-07-23 22:49:13 +0530

    init

---
+++ src/lib/components/landing/Atmosphere.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import data_platform from '$lib/assets/data_platform.png';
+	import apps from '$lib/assets/apps.png'
+	import group from '$lib/assets/group.png'
+</script>
+
+<h2>Welcome to the Atmosphere</h2>
+
+<p>
+	Better way to understand this would be a Camper. Camper is a travelling home vehicle that you can
+	take anywhere. It provides you the freedom of living wherever you wish. Same is true for any app
+	made on AT protocol.
+</p>
+
+<p>
+	If you don't like it, you can always pack up and go somewhere else. But Big Tech doesn't like the
+	idea. They want you to hand over the keys so they can decide where you settle.
+</p>
+
+<p>
+	Ask any camper owner, the best thing about owning one is freedom. This freedom allows to take part
+	in anything you find interesting, and for Roomy that means a huge ecosystem of apps built on AT
+	Protocol.
+</p>
+
+<img src={data_platform} class="cripsy" alt="two groups, one is group of folks in a circle 'data' and other being 'platform' " />
+
+<p>
+    You always own your data. You choose a trusted location to settle it while being part of the community.
+</p>
+
+<img src={apps} class="cripsy" alt="closed circles with famous social media apps, representing their nature of locking userdata at one place on their platform." />
+
+<img src={group} class="cripsy" alt="open nature of at-protocol allowing many apps to coexist and share their data from a single source." />
+
+<p class="mb-30">
+    Roomy provides you the way to design your community while tinkering with apps you like from the ecosystem. Oh, also did we mention that you can use one account to use all of this?
+</p>
