16c14872e498dba8734520fa52e914b00d860302
Jaydip Sanghani <91427591+jellydeck@users.noreply.github.com>
2026-08-01 09:33:35 +0530

    forgot to attach T T

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		enhancedImages(),
-		tailwindcss(),
-		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
-			adapter: adapter(),
-			paths: {
-				base: process.env.BASE_PATH ?? ''
-			}
-		})
-	]
+	plugins: [enhancedImages(), tailwindcss(), sveltekit()]
 });
